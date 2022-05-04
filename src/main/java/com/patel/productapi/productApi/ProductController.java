7eb9c005c5be99a23c06f5b63dd3efbfe78e5323
Patel <patrickngare@gmail.com>
2022-05-04 22:02:41 +0300

    Product api product Api

--- src/main/java/com/patel/productapi/productApi/ProductController.java
+++ src/main/java/com/patel/productapi/productApi/ProductController.java
@@ -4,16 +4,11 @@ package com.patel.productapi.productApi;
 import com.patel.productapi.models.Product;
 import com.patel.productapi.repos.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@ComponentScan
 public class ProductController {
 
     @Autowired
@@ -32,13 +27,13 @@ public class ProductController {
         return repository.findById(id).get();
     }
     @RequestMapping(value="/products/",method = RequestMethod.POST)
-    public Product createProduct(Product product){
+    public Product createProduct(@RequestBody Product product){
 
         return repository.save(product);
     }
 
     @RequestMapping(value="/products/",method = RequestMethod.PUT)
-    public Product updateProduct(Product product){
+    public Product updateProduct(@RequestBody Product product){
 
         return repository.save(product);
     }
