f5af1162d70ad3c89c32906a517c1fc3b4d3be04
Patel <patrickngare@gmail.com>
2022-05-11 19:10:45 +0300

    Debugged the error to connect the database

--- src/main/java/com/patel/productapi/repos/ProductRepository.java
+++ src/main/java/com/patel/productapi/repos/ProductRepository.java
@@ -4,7 +4,15 @@ import com.patel.productapi.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer > {
+
+
+
+
+
+
 
 }
