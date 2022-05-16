316a424d60731d1909269a3799e5adececc6c9a2
Patel <patrickngare@gmail.com>
2022-05-16 18:38:27 +0300

    added rest template get method

--- src/test/java/com/patel/productapi/ProductApiApplicationTests.java
+++ src/test/java/com/patel/productapi/ProductApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.patel.productapi;
 
+import com.patel.productapi.models.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.Assert.*;
+
 
 @SpringBootTest
 class ProductApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testGetProduct() {
+        RestTemplate restTemplate = new RestTemplate();
+        Product product= restTemplate.getForObject("http://localhost:8901/ShopApi/products/", Product.class);
+        assertNotNull(product);
+        assertEquals("Sugar",product.getName());
+
     }
 
 }
