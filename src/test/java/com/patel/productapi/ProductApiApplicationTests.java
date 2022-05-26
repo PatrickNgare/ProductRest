01443a2da37e53b45469bad58c665010a98c17c4
Patel <patrickngare@gmail.com>
2022-05-26 19:51:08 +0300

    adding profile config

--- src/test/java/com/patel/productapi/ProductApiApplicationTests.java
+++ src/test/java/com/patel/productapi/ProductApiApplicationTests.java
@@ -2,6 +2,7 @@ package com.patel.productapi;
 
 import com.patel.productapi.models.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 import static org.junit.Assert.*;
@@ -10,10 +11,14 @@ import static org.junit.Assert.*;
 @SpringBootTest
 class ProductApiApplicationTests {
 
+
+    @Value("${productrestapi.services.url}")
+    private String baseUrl;
+
     @Test
     public void testGetProduct() {
         RestTemplate restTemplate = new RestTemplate();
-        Product product= restTemplate.getForObject("http://localhost:8901/ShopApi/products/1", Product.class);
+        Product product= restTemplate.getForObject(baseUrl+ "1", Product.class);
         assertNotNull(product);
         assertEquals("Sugar " ,product.getName());
 
@@ -22,10 +27,11 @@ class ProductApiApplicationTests {
     public void testCreateProduct() {
         RestTemplate restTemplate = new RestTemplate();
         Product product=new Product();
+        product.setId(2);
         product.setName("Samsung Mobile");
         product.setDescription("It is awesome");
         product.setPrice(1000);
-       Product newproduct= restTemplate.postForObject("http://localhost:8901/ShopApi/products/",product,Product.class);
+       Product newproduct= restTemplate.postForObject(baseUrl ,product,Product.class);
        assertNotNull(newproduct);
        assertNotNull(newproduct.getId());
        assertEquals("Samsung Mobile",newproduct.getName());
